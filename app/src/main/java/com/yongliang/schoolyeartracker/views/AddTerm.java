3c3f341326ff736203ba796709ebe48d2edd1239
‘yongliang24’ <yongliang24@gmail.com>
2021-11-13 07:52:34 -0800

    implemented add term

--- app/src/main/java/com/yongliang/schoolyeartracker/views/AddTerm.java
+++ app/src/main/java/com/yongliang/schoolyeartracker/views/AddTerm.java
@@ -6,12 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
+import com.yongliang.schoolyeartracker.Database.Repository;
+import com.yongliang.schoolyeartracker.Entity.TermEntity;
 import com.yongliang.schoolyeartracker.R;
 
 import java.util.Objects;
 
 public class AddTerm extends AppCompatActivity {
+    EditText addTerm;
+    String termTitle;
+
+    DatePicker sDate;
+    String startDate;
+
+    DatePicker eDate;
+    String endDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +44,31 @@ public class AddTerm extends AppCompatActivity {
     }
 
     public void saveTerm(View view) {
+        addTerm=findViewById(R.id.addTermTitle);
+        termTitle=addTerm.getText().toString();
+        //System.out.println("term title: "+termTitle);
 
-        Intent intent = new Intent(AddTerm.this, TermActivity.class);
-        startActivity(intent);
+        //convert date picker to string
+        sDate=findViewById(R.id.startDatePicker);
+        startDate=String.valueOf(sDate.getMonth())+"-"+String.valueOf(sDate.getDayOfMonth())+"-"+String.valueOf(sDate.getYear());
+        //System.out.println("date: "+startDate);
+
+        eDate=findViewById(R.id.endDatePicker);
+        endDate=String.valueOf(eDate.getMonth())+"-"+String.valueOf(eDate.getDayOfMonth())+"-"+String.valueOf(eDate.getYear());
+        //System.out.println("date: "+endDate);
+
+        //validate term title input
+        if(termTitle.isEmpty()){
+            addTerm.setHint("This field is Required.");
+        }
+        else{
+            Repository repo = new Repository(getApplication());
+            TermEntity newTerm= new TermEntity(termTitle,startDate,endDate);
+            repo.insert(newTerm);
+
+            Intent intent = new Intent(AddTerm.this, TermActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/com/yongliang/schoolyeartracker/views/MainActivity.java
+++ app/src/main/java/com/yongliang/schoolyeartracker/views/MainActivity.java
@@ -19,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Repository repo = new Repository(getApplication());
-        TermEntity term1 = new TermEntity("summer","09-08-2021","12-09-2021 ");
-        CourseEntity course1 = new CourseEntity(1, "math","03/01","06/05","complete","meme","999-9999-9993","xx@g,ail.com","first note", false);
-        AssessmentEntity assessment1 = new AssessmentEntity(1, "objective", "new assessment", "06-07-2021");
-
+//        Repository repo = new Repository(getApplication());
+//        TermEntity term1 = new TermEntity("summer","09-08-2021","12-09-2021 ");
+//        CourseEntity course1 = new CourseEntity(1, "math","03/01","06/05","complete","meme","999-9999-9993","xx@g,ail.com","first note", false);
+//        AssessmentEntity assessment1 = new AssessmentEntity(1, "objective", "new assessment", "06-07-2021");
+//
 //        repo.insert(term1);
 //        repo.insert(course1);
 //        repo.insert(assessment1);
