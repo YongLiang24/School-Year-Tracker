58a3eb274ddbec4d9d9b012c580ede52808b2b74
‘yongliang24’ <yongliang24@gmail.com>
2021-11-20 20:27:11 -0800

    added menu for assessment

--- app/src/main/java/com/yongliang/schoolyeartracker/views/AssessmentActivity.java
+++ app/src/main/java/com/yongliang/schoolyeartracker/views/AssessmentActivity.java
@@ -2,7 +2,9 @@ package com.yongliang.schoolyeartracker.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.yongliang.schoolyeartracker.R;
@@ -25,7 +27,24 @@ public class AssessmentActivity extends AppCompatActivity {
             case android.R.id.home:
                 this.finish();
                 return true;
+
+            case R.id.share_note:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "populate this text");
+                sendIntent.putExtra(Intent.EXTRA_TITLE, "Share this note");
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent=Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        //inflate the menu items
+        getMenuInflater().inflate(R.menu.menu01, menu);
+        return true;
     }
 }
