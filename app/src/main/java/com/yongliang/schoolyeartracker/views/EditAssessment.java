7f255abe4fd8e1480e73e06c67f2b822a29122ec
‘yongliang24’ <yongliang24@gmail.com>
2021-11-27 20:21:23 -0800

    v1.0 completed

--- app/src/main/java/com/yongliang/schoolyeartracker/views/EditAssessment.java
+++ app/src/main/java/com/yongliang/schoolyeartracker/views/EditAssessment.java
@@ -8,19 +8,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.yongliang.schoolyeartracker.Database.Repository;
 import com.yongliang.schoolyeartracker.Entity.AssessmentEntity;
+import com.yongliang.schoolyeartracker.Entity.CourseEntity;
 import com.yongliang.schoolyeartracker.R;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class EditAssessment extends AppCompatActivity {
 
     private Repository repo=new Repository(getApplication());
-
     int a_id;
     AssessmentEntity astObj;
+
+    EditText aTitle;
+    Spinner aSpinner;
+    DatePicker eDate;
+
+
+    EditText cName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,38 @@ public class EditAssessment extends AppCompatActivity {
         a_id=extraInfo.getInt("assessment_id");
         astObj=repo.getThisAssessment(a_id);
 
+        aTitle = findViewById(R.id.a_title);
+        aSpinner = findViewById(R.id.a_spinner);
+        eDate=findViewById(R.id.a_endDatePicker);
+
+        aTitle.setText(astObj.getAssessmentTitle());
+
+        //set default value for spinner
+        ArrayAdapter<CharSequence> adapt =ArrayAdapter.createFromResource(this, R.array.type_spinner, android.R.layout.simple_spinner_item);
+        adapt.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        aSpinner.setAdapter(adapt);
+        String spinArray [] = getResources().getStringArray(R.array.type_spinner);
+        int spinIndex =getSpinnerIndex(spinArray, astObj);
+        //mSpinner.getSelectedItem();
+        aSpinner.setSelection(spinIndex);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        //convert String to LocalDate
+        LocalDate e_date = LocalDate.parse(astObj.getEndDate(), formatter);
+        eDate.updateDate(e_date.getYear(), e_date.getMonthValue()+1, e_date.getDayOfYear());
+
+    }
+
+    //get index for spinner item preload
+    int getSpinnerIndex(String str[], AssessmentEntity aObj){
+        int index=0;
+        for(String status: str){
+            if(status.equals(aObj.getAssessmentType())){
+                return index;
+            }
+        }
+        index++;
+        return 0;
     }
 
     @Override
@@ -46,6 +91,18 @@ public class EditAssessment extends AppCompatActivity {
     }
 
     public void updateAssessment(View view) {
+        astObj.setAssessmentTitle(aTitle.getText().toString());
+        astObj.setAssessmentType(aSpinner.getSelectedItem().toString());
+        //format date
+        int eMonth = eDate.getMonth()+1;
+        String endDate= String.valueOf(eMonth)+"/"+String.valueOf(eDate.getDayOfMonth())+"/"+String.valueOf(eDate.getYear());
+        astObj.setEndDate(endDate);
+        //update assessment
+        repo.update(astObj);
+
+        Intent intent =new Intent(EditAssessment.this, AssessmentActivity.class);
+        intent.putExtra("course_id", astObj.getCourse_id());
+        startActivity(intent);
     }
 
     public void deleteAssessment(View view) {
